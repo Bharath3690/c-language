7435007b4da0e2d84e85a04d790d3798ee5ad588
Bharath <115339747+Bharath3690@users.noreply.github.com>
2023-03-26 22:29:54 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int r;
+		float p=3.14,area;
+	printf("enter the value r");
+	scanf("%d",&r);
+
+
+	area=(p*r*r);
+	
+	printf("ara of  circle: %f",area);
+	return 0;
+}
